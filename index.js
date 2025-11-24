fc6afc749f6deecf476296165a78a9cb956d65a2
Gustavo <gustavodabreus@gmail.com>
2025-11-24 17:03:36 -0300

    atualização de parametro

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const printer = require("pdf-to-printer");
 
+// Primeiro argumento = caminho do PDF
 const file = process.argv[2];
+// Segundo argumento = nome/endereço da impressora (ex.: "\\\\n050fp01\\Central")
+const printerName = process.argv[3];
 
 if (!file) {
-  console.error("Uso: node index.js <arquivo.pdf>");
+  console.error("Uso: node index.js <arquivo.pdf> [nome_da_impressora]");
   process.exit(1);
 }
 
-printer.print(file)
-  .then(() => console.log(`Arquivo ${file} enviado para a impressora padrão com sucesso!`))
+printer.print(file, printerName ? { printer: printerName } : {})
+  .then(() => {
+    console.log(`Arquivo ${file} enviado para ${printerName || "impressora padrão"} com sucesso!`);
+  })
   .catch(err => console.error("Erro ao imprimir:", err));
